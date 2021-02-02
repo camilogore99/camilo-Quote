1e5fcb1ee661dc6ddf50cb2294c799bea0cc2615
camilo <camilogore99@gmail.com>
2021-02-01 19:30:19 -0500

    ready

--- src/componentes/logo.js
+++ src/componentes/logo.js
@@ -3,7 +3,7 @@ import { FaTwitter } from 'react-icons/fa'
 const Logo = ({color}) => {
       return(
             <div className="icon">
-            <a style={{background:''+color+''}} className="icon-twi" href="https://twitter.com/compose/tweet">< FaTwitter/></a>
+            <a style={{background:''+color+''}} className="icon-twi" href="https://twitter.com/compose/tweet" target="_blank ">< FaTwitter/></a>
             </div>
       )
 }
